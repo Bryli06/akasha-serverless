029e69fc42ba9c929685d00f3bf6679945b25bed
Bryan Li <mz.bryan.li@gmail.com>
2022-10-14 22:52:14 -0700

    finished weapon

--- src/commands/chances.rs
+++ src/commands/chances.rs
@@ -1,7 +1,7 @@
 use std::cmp;
 
 use async_trait::async_trait;
-use ndarray::{Array1, s, Array2, Axis, Slice};
+use ndarray::{Array1, s, Array2, Axis, Slice, arr2, arr1};
 use worker::console_log;
 use crate::{discord::*, utils::InteractionError};
 use serde_json::value::Value;
@@ -51,7 +51,7 @@ impl Command for Chances {
                 required: Some(true),
                 option_type: ApplicationCommandOptionType::Integer,
                 choices: None,  
-                min_value: Some(0),
+                min_value: Some(1),
                 max_value: Some(1260),
                 min_length: None,
                 max_length: None,
@@ -113,7 +113,7 @@ impl Command for Chances {
                 input.get_options("pity").unwrap().as_u64().unwrap(), 
                 input.get_options("guarentee").unwrap().as_u64().unwrap() as usize,),
             Some(1) => five_star_weapon(
-                input.get_options("wishes").unwrap().as_u64().unwrap(), 
+                input.get_options("wishes").unwrap().as_u64().unwrap() as usize, 
                 input.get_options("pity").unwrap().as_u64().unwrap(),),
             _ => {
                 console_log!("Unknown banner");
@@ -172,7 +172,6 @@ fn five_star_character(wishes: usize, pity: u64, guarentee: usize) -> Embed {
     gf_coefficents.index_axis_mut(Axis(0), 0).slice_mut(s![(pity+1) as i32 .. 91]).assign(&(&base_gf_coefficents.slice(s![(pity+1) as i32 .. 91])/(1.0 - pity_sum)));
 
     for i in 1..14 {
-        println!("{:?}", gf_coefficents);
         for j in 1..cmp::min(90*i+1, wishes+pity as usize) {
             let temp = gf_coefficents[[i-1, j]];
             gf_coefficents.index_axis_mut(Axis(0), i).slice_mut(s![j as i32 .. (j + 91) as i32]).scaled_add(temp, &base_gf_coefficents);
@@ -187,7 +186,7 @@ fn five_star_character(wishes: usize, pity: u64, guarentee: usize) -> Embed {
         for j in 0..(i+2-guarentee) {
             temp += 100.0 * (1..=j.min((i + 1 - guarentee) - j))
                 .fold(1, |acc, val| acc * ((i + 1 - guarentee) - val + 1) / val) as f64 
-                / (2 << (i - guarentee)) as f64 * five_star_prob[i+j];
+                / (1 << (i + 1 - guarentee)) as f64 * five_star_prob[i+j];
         }
         cons.push(EmbedField { 
             name: format!("C{}", i), 
@@ -196,7 +195,6 @@ fn five_star_character(wishes: usize, pity: u64, guarentee: usize) -> Embed {
         })
     }
 
-    console_log!("{:?}", five_star_prob);
     Embed {
         title: Some("Character chances calculator".to_string()),
         embed_type: Some(EmbedType::Rich),
@@ -210,34 +208,85 @@ fn five_star_character(wishes: usize, pity: u64, guarentee: usize) -> Embed {
     }
 }
 
-fn five_star_weapon(wishes: u64, pity: u64) -> Embed {
+fn five_star_weapon(wishes: usize, pity: u64) -> Embed {
+    let P = 0.007;
+    let ramp_rate = 0.07;
+
+    let base_gf_coefficents = { //scope so it gets dropped
+        let mut cum_prob = Array1::<f64>::zeros(78);
+
+        cum_prob[0] = 0.0;
+        cum_prob.slice_mut(s![1..63 as i32]).fill(P);
+        cum_prob[77] = 1.0;
+
+        for i in 63..77 {
+            cum_prob[i] = P + ramp_rate * (i-62) as f64;
+        }
+
+        let mut temp = Array1::<f64>::zeros(78);
+        let mut cum_product = 1.0;
+        for i in 0..78 {
+            temp[i] = cum_product * cum_prob[i];
+            cum_product *= 1.0 - cum_prob[i];
+        }
+        temp
+    };
+    let pity_sum = base_gf_coefficents.slice(s![1.. (pity+1) as i32]).sum();
+
+    let mut gf_coefficents = Array2::<f64>::zeros((15, pity as usize + wishes + 79));
+
+    gf_coefficents.index_axis_mut(Axis(0), 0).slice_mut(s![(pity+1) as i32 .. 78]).assign(&(&base_gf_coefficents.slice(s![(pity+1) as i32 .. ])/(1.0 - pity_sum)));
+
+    for i in 1..15 {
+        for j in 1..cmp::min(77*i+1, wishes+pity as usize) {
+            let temp = gf_coefficents[[i-1, j]];
+            gf_coefficents.index_axis_mut(Axis(0), i).slice_mut(s![j as i32 .. (j + 78) as i32]).scaled_add(temp, &base_gf_coefficents);
+        }
+    }
+
+    let five_star_prob = gf_coefficents.slice(s![.., ..(wishes+pity as usize+1)]).sum_axis(Axis(1));
+
+    let mut path_gf_coefficents = Array2::<f64>::zeros((5, 16));
+    
+    path_gf_coefficents.index_axis_mut(Axis(0), 0).slice_mut(s![0i32..4]).assign(&arr1(&[0.0, 0.375, 0.265625, 0.359375]));
+
+    for i in 1..5 {
+        for j in 1..3*i+1 {
+            let temp = path_gf_coefficents[[i-1, j]];
+
+            path_gf_coefficents.index_axis_mut(Axis(0), i).slice_mut(s![j as i32 .. (j + 4) as i32]).scaled_add(temp, &arr1(&[0.0, 0.375, 0.265625, 0.359375]));
+        }
+    }
+
+    let mut cons: Vec<EmbedField> = Vec::new(); 
+    for i in 0..5 {
+        cons.push(EmbedField { 
+            name: format!("C{}", i), 
+            value: round_sigfig(100.0 * path_gf_coefficents.index_axis_mut(Axis(0), i).slice(s![1i32..])
+                .dot(&five_star_prob), 4), 
+            inline: Some(true), 
+        })
+    }
+
     Embed {
-        title: Some("Hello".to_string()),
+        title: Some("Weapon chances calculator".to_string()),
         embed_type: Some(EmbedType::Rich),
-        description: Some("E".to_string()),
+        description: Some("If you wish to understand the math behind this calculation, view the explanation [here](https://drive.google.com/file/d/1EECcjNVpfiOTqRoS48hHWqH2Ake902vq/view?usp=sharing)".to_string()),
         url: None,
         color: Some(0x696969),
         footer: None,
         image: None,
         thumbnail: None, 
-        fields: Some(vec![
-            EmbedField {
-                name: "Wishes".to_string(),
-                value: format!("{}", wishes),
-                inline: Some(true),
-            }, 
-            EmbedField {
-                name: "Pity".to_string(),
-                value: format!("{}", pity),
-                inline: Some(true),
-            },
-        ]),
+        fields: Some(cons),
     }
 }
 
 fn round_sigfig(num: f64, sigfigs: isize) -> String {
+    if num == 0.0 {
+        return "0.0000%".to_string()
+    }
     let leading_digits = num.abs().log10().ceil() as isize; // Treat fractions as negative leading digits so we have the correct total number of significant digits
     let trailing_digits = if leading_digits > sigfigs { 0 } else { (sigfigs - leading_digits) as usize };
     
-    format!("{:.*}", trailing_digits, num)
+    format!("{:.*}%", trailing_digits, num)
 }
